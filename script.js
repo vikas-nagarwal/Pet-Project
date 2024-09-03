9e07dc679a5bb820e5ee9a69388f6f01e26f3a0c
vikas nagarwal <161242717+vikas-nagarwal@users.noreply.github.com>
2024-09-03 16:50:41 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+  
+function myFunction() {
+  var x = document.getElementById("topnav");
+  if (x.className === "ulsec") {
+    x.className += "responsive";
+  } else {
+    x.className = "ulsec";
+  }
+}
+
+
